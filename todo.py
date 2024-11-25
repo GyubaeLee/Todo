950269ac404f98be648901791f2c38ce75df51e5
GyubaeLee <igyubae@gmail.com>
2024-11-25 01:23:24 -0800

    작업관리자만들기

---
+++ todo.py
@@ -0,0 +1,13 @@
+def show_menu(): #메뉴를 보여주는 함수
+    print("작업 관리 어플리케이션")
+    print("1. 할 일 추가")
+    print("2. 할 일 목록보기")
+    print("3. 할 일 완료")
+    print("4. 할 일 삭제")
+    print("5. 종료")
+    
+def main():
+    while True:
+        show_menu()
+        
+main()
